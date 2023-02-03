2900eb7bf1cc3937ee4e21adeb377eab464a11cf
lixiao189 <44799572+lixiao189@users.noreply.github.com>
2023-02-03 07:20:00 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/site.config.ts
@@ -0,0 +1,12 @@
+const siteMeta = {
+	author: "Chris Williams",
+	title: "Astro Theme Cactus",
+	description: "An opinionated starter theme for Astro",
+	lang: "en-GB",
+	ogLocale: "en_GB",
+	githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
+	themeColorLight: "#fafafa",
+	themeColorDark: "#1d1f21",
+};
+
+export default siteMeta;
