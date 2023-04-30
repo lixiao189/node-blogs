b79b112a33be887c2dedf5336f64dac8b7c8df35
node189 <node189@icloud.com>
2023-04-30 11:42:30 +0800

    feat: improve the rss experience

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
 import siteMeta from "@/site-config";
+
+const parser = new MarkdownIt();
 
 export const get = async () => {
 	const posts = await getCollection("post");
@@ -8,10 +12,12 @@ export const get = async () => {
 	return rss({
 		title: siteMeta.title,
 		description: siteMeta.description,
+		stylesheet: "/rss-style.xsl",
 		site: import.meta.env.SITE,
 		items: posts.map((post) => ({
 			title: post.data.title,
 			description: post.data.description,
+			content: sanitizeHtml(parser.render(post.body)),
 			pubDate: post.data.publishDate,
 			link: "/posts/" + post.slug,
 		})),
